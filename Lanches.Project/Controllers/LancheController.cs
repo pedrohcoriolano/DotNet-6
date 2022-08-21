ebc80bef0f42b05db664f6ae40c3298d38fad997
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-08-21 18:56:35 -0300

    Avançando com o curso

---
+++ Lanches.Project/Controllers/LancheController.cs
@@ -0,0 +1,29 @@
+﻿using Lanches.Project.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Project.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lanchesRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lanchesRepository = lancheRepository;
+        }
+        public IActionResult List()
+        {
+            ViewData["Titulo"] = "Todos os lanches";
+            ViewData["Data"] = DateTime.Now;
+
+            var lanches = _lanchesRepository.Lanches;
+
+            var totalLanches = lanches.Count();
+            ViewBag.Total = "Total de lanches:";
+            ViewBag.TotalLanches = totalLanches;
+
+
+            return View(lanches);
+        }
+    }
+}
