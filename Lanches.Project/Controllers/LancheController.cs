8d1201e35c7cd4b53a0aa06fb6a1dad946feb244
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohocc@users.noreply.github.com>
2022-08-29 14:31:05 -0300

    avançando

--- Lanches.Project/Controllers/LancheController.cs
+++ Lanches.Project/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using Lanches.Project.Repositories.Interfaces;
+using Lanches.Project.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lanches.Project.Controllers
@@ -13,17 +14,23 @@ namespace Lanches.Project.Controllers
         }
         public IActionResult List()
         {
-            ViewData["Titulo"] = "Todos os lanches";
-            ViewData["Data"] = DateTime.Now;
+            //ViewData["Titulo"] = "Todos os lanches";
+            //ViewData["Data"] = DateTime.Now;
 
-            var lanches = _lanchesRepository.Lanches;
+            //var lanches = _lanchesRepository.Lanches;
 
-            var totalLanches = lanches.Count();
-            ViewBag.Total = "Total de lanches:";
-            ViewBag.TotalLanches = totalLanches;
+            //var totalLanches = lanches.Count();
+            //ViewBag.Total = "Total de lanches:";
+            //ViewBag.TotalLanches = totalLanches;
 
 
-            return View(lanches);
+            //return View(lanches);
+
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.lancheList = _lanchesRepository.Lanches;
+            lancheListViewModel.categoriaAtual = "Categoria atual";
+
+            return View(lancheListViewModel);
         }
     }
 }
---
+++ Lanches.Project/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Lanches.Project.Models;
+
+namespace Lanches.Project.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> lancheList { get; set; }
+
+        public string categoriaAtual { get; set; }
+    }
+}
